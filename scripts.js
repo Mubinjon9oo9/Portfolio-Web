757ac938f31297cd646f6d704f60fffa87b2734e
Mubinjon Akilzhanov <mubinjon9009@mail.ru>
2024-07-02 19:39:02 +0700

    customized dark mode

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 var bg = "#fff"
 function modeSwitch(){
     if (bg=="#fff"){
-        document.documentElement.style.setProperty("--bg-color", "#000");
+        document.documentElement.style.setProperty("--bg-color", "#373737");
         document.documentElement.style.setProperty("--text-color", "#fff");
         bg = "#000"
     }else{
@@ -10,3 +10,12 @@ function modeSwitch(){
         bg = "#fff"
     }
 }
+function getUserColorScheme() {
+    if (window.matchMedia) {
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.getElementById("scroll").checked=true;
+        modeSwitch();
+      } 
+    } 
+  }
+getUserColorScheme();
